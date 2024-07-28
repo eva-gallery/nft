de1e61b165294be802f5f940acdfde647401a65e
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2024-07-28 20:23:20 +0200

    change chain

--- src/nftMetadata/metadata.service.ts
+++ src/nftMetadata/metadata.service.ts
@@ -5,7 +5,7 @@ import { getClient } from '@kodadot1/uniquery'
 @Injectable()
 export class nftModuleService {
   async getAccountNFTMetadata(account: string): Promise<any> {
-    const client = getClient('ahk' as any)
+    const client = getClient('ahp' as any)
     const query = client.itemListByOwner(account)
   
     console.log(JSON.stringify(query, null, 2))
